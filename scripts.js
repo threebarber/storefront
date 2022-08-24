827cd744840dd5449b2ed1503864e4ea313b9acc
threebarber <dvtdropshot@gmail.com>
2022-08-23 22:05:12 -0400

    initial commit 2

---
+++ scripts.js
@@ -0,0 +1,13 @@
+const product = function(id,title,price,description,category,image,rating) {
+    /*this.sayHello = () => console.log('hello!');*/
+
+    this.id = id;
+    this.title = title;
+    this.price = price;
+    this.description = description;
+    this.category = category;
+    this.image = image;
+    this.rating = rating;
+  };
+  
+  
